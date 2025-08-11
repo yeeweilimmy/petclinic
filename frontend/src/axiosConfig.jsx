92de47e8d2961d0f100a2e39f68575f8bb4673ca
yeeweilimmy <yeewei.lim1@gmail.com>
2025-08-11 17:01:07 +1000

    fix base url

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://54.252.39.116.131:5001', // live
+  baseURL: 'http://54.252.39.116:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
