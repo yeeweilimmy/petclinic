27725dc88d5d9e96085d937fed26d985dca6afef
yeeweilimmy <yeewei.lim1@gmail.com>
2025-08-11 16:18:30 +1000

    changes

--- backend/test/example_test.js
+++ backend/test/example_test.js
@@ -2,17 +2,19 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const http = require('http');
-const app = require('../server'); 
+const app = require('../server');
 const connectDB = require('../config/db');
 const mongoose = require('mongoose');
 const sinon = require('sinon');
 const Task = require('../models/Task');
-const { updateTask,getTasks,addTask,deleteTask } = require('../controllers/taskController');
+const { updateTask, getTasks, addTask, deleteTask } = require('../controllers/taskController');
 const { expect } = chai;
 
 chai.use(chaiHttp);
 let server;
 let port;
+
+//test
 
 
 describe('AddTask Function Test', () => {
@@ -100,7 +102,7 @@ describe('Update Function Test', () => {
       body: { title: "New Task", completed: true }
     };
     const res = {
-      json: sinon.spy(), 
+      json: sinon.spy(),
       status: sinon.stub().returnsThis()
     };
 
