3257ae3062c3f7c97ce8cd4d3acc3adb092c59ef
yeeweilimmy <yeewei.lim1@gmail.com>
2025-08-14 18:08:18 +1000

    PC-32 frontend edit button

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -27,6 +27,7 @@ const TaskList = ({ tasks, setTasks, setEditingTask }) => {
             {new Date(task.deadline).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
           </p>
           <div className="mt-2">
+            {/* edit button */}
             <button
               onClick={() => setEditingTask(task)}
               className="mr-2 bg-yellow-500 text-white px-4 py-2 rounded"
