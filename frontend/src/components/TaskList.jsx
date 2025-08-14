5463ba263c86e26393eaba790a9269acd0101da4
yeeweilimmy <yeewei.lim1@gmail.com>
2025-08-14 18:11:54 +1000

    PC-34 frontend delete button

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -34,6 +34,7 @@ const TaskList = ({ tasks, setTasks, setEditingTask }) => {
             >
               Edit
             </button>
+            {/* delete button */}
             <button
               onClick={() => handleDelete(task._id)}
               className="bg-red-500 text-white px-4 py-2 rounded"
