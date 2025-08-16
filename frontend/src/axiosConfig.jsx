1b8fcb8ce779054a09bb267a23b2a1045043efcd
yeeweilimmy <yeewei.lim1@gmail.com>
2025-08-17 09:16:04 +1000

    ip change

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:5001', // locals
-  baseURL: 'http://13.211.86.56:5001',// live
+  // baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://16.176.181.46:5001',// live 
   headers: { 'Content-Type': 'application/json' },
 });
 
