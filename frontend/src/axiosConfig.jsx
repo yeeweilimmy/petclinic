a7e3e9f4f539d304f453f642be193fa2227e1a7a
yeeweilimmy <yeewei.lim1@gmail.com>
2025-08-25 14:23:15 +1000

    test

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://localhost:5001', // local
 
-  baseURL: 'http://13.239.135.228:5001',// live.
+  // baseURL: 'http://13.239.135.228:5001',// live.
 
   headers: { 'Content-Type': 'application/json' },
 });
